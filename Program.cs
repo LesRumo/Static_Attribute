9f0aa7e988c4c8bef19b7d78d11ecb845aad3ddb
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-05 16:22:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //its an attribute on the class itself 
+            Songs holiday = new Songs("Holiday", "Green Day", 2);
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin", 4);
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
